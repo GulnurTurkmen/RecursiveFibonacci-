ce6828c823178dd88c1944da3e9294a65ef6262f
GÜLNUR TÜRKMEN <89343746+GulnurTurkmen@users.noreply.github.com>
2024-02-13 20:23:10 +0300

    Update RecursiveFibonacci.java

--- src/RecursiveFibonacci/RecursiveFibonacci.java
+++ src/RecursiveFibonacci/RecursiveFibonacci.java
@@ -1,5 +1,13 @@
 package RecursiveFibonacci;
-
+import java.util.Scanner;
 public class RecursiveFibonacci {
-
+static int fib(int n) {
+	if(n==1 || n==2) {
+		return 1;
+	}
+	return fib(n-1)+fib(n-2);
+}
+	public static void main(String[]args) {
+		System.out.println(fib(5));
+	}
 }
